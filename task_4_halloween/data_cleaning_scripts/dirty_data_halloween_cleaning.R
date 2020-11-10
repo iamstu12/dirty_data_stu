89c46de6963df2385ab9548dc7a2af4d098264da
Stuart McColl <StuartCMcColl@gmail.com>
2020-11-10 12:20:20 +0000

    Loaded in Halloween Data

---
+++ task_4_halloween/data_cleaning_scripts/dirty_data_halloween_cleaning.R
@@ -0,0 +1,22 @@
+# 1.4 Task 4 - Halloween Candy Data ----
+
+# Step 1 - Load Libraries ----
+
+library(tidyverse)
+library(readxl)
+library(readr)
+library(janitor)
+library(here)
+
+here::here()
+
+# Step 2 - Load Data 
+
+bb_candy_2015 <- read_excel("/Users/Natifu/dirty_data_stu/task_4_halloween/raw_data/boing-boing-candy-2015.xlsx")
+bb_candy_2016 <- read_excel("/Users/Natifu/dirty_data_stu/task_4_halloween/raw_data/boing-boing-candy-2016.xlsx")
+bb_candy_2017 <- read_excel("/Users/Natifu/dirty_data_stu/task_4_halloween/raw_data/boing-boing-candy-2017.xlsx")
+
+
+
+
+
